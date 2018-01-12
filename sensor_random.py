c1e5a6456b3112e32313e099c87c5ca3f394c288
porcupal <michael.d.sanchez@gmail.com>
2018-01-12 12:17:08 -0800

    dec 12 - added both user credentials

--- sensor_random.py
+++ sensor_random.py
@@ -8,7 +8,13 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 # AWS IoT certificate based connection
 myMQTTClient = AWSIoTMQTTClient("123afhlss456")					#This is just an ID so that the MQTT broker can identify the client, using any random string will do.
 myMQTTClient.configureEndpoint("a375a74rjdbru6.iot.us-west-2.amazonaws.com", 8883)			#this is the unique thing endpoint with the .503 certificate
+# giovanni
 myMQTTClient.configureCredentials("aws-iot-device-sdk-python/deviceSDK/certs/VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem.txt", "aws-iot-device-sdk-python/deviceSDK/certs/9e6528d5bc-private.pem.key", "aws-iot-device-sdk-python/deviceSDK/certs/9e6528d5bc-certificate.pem.crt")
+# michael
+myMQTTClient.configureCredentials(
+    "aws-iot-device-sdk-python/deviceSDK/certs/VeriSign-Class3-Public-Primary-Certification-Authority-G5.pem.txt",
+    "aws-iot-device-sdk-python/deviceSDK/certs/24535dbd29-private.pem.key",
+    "aws-iot-device-sdk-python/deviceSDK/certs/24535dbd29-certificate.pem.crt")
 #above has the path for the CA root cert, private key cert, and device cert
 myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
@@ -25,7 +31,7 @@ now_str = date.strftime('%m/%d/%Y %H:%M:%S %Z')
 
 payload = {
     'datetime': now_str,
-    'location': 'default'
+    'location': 'default',
     'temp': 0.0,
     'humid': 0.0,
     'light': 0.0
@@ -34,7 +40,6 @@ payload = {
 def rand_sensor_data():
     print('randomizing sensor data')
     for each in payload:
-        each = randint(1, 51)
     print('complete')
 
 try:
--- sensor_reader.py
+++ sensor_reader.py
@@ -20,7 +20,13 @@ GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # AWS IoT certificate based connection
 myMQTTClient = AWSIoTMQTTClient("123afhlss456")					#This is just an ID so that the MQTT broker can identify the client, using any random string will do.
 myMQTTClient.configureEndpoint("a375a74rjdbru6.iot.us-west-2.amazonaws.com", 8883)			#this is the unique thing endpoint with the .503 certificate
+# giovanni
 myMQTTClient.configureCredentials("aws-iot-device-sdk-python/deviceSDK/certs/VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem.txt", "aws-iot-device-sdk-python/deviceSDK/certs/9e6528d5bc-private.pem.key", "aws-iot-device-sdk-python/deviceSDK/certs/9e6528d5bc-certificate.pem.crt")
+# michael
+myMQTTClient.configureCredentials(
+    "aws-iot-device-sdk-python/deviceSDK/certs/VeriSign-Class3-Public-Primary-Certification-Authority-G5.pem.txt",
+    "aws-iot-device-sdk-python/deviceSDK/certs/24535dbd29-private.pem.key",
+    "aws-iot-device-sdk-python/deviceSDK/certs/24535dbd29-certificate.pem.crt")
 #above has the path for the CA root cert, private key cert, and device cert
 myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
